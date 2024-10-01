03fc0468fe5c5215eb6f91b5056c6cd6a943fc22
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-02 00:48:17 +0900

    Refactor footer.blade.php to use flexbox for centering and adjust height and text size

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="w-full bg-blue-950 text-white">
-    <p class="text-center py-4">&copy; {{ $year }} Yasuhiro WATANABE All rights reserved.</p>
+<div class="w-full h-10 bg-blue-950 flex justify-center items-center">
+    <p class="text-sm text-center text-white">&copy; {{ $year }} Yasuhiro WATANABE All rights reserved.</p>
 </div>
