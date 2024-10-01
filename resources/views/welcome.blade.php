c2d3bff0047e937ac92fe8d2b37abfa1665877a1
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-01 19:41:39 +0900

    Refactor welcome.blade.php to extend layout.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Top Page</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="bg-gray-100">
+@section('content')
     <div class="container mx-auto p-6">
         <h1 class="text-4xl font-bold text-center">トップページへようこそ</h1>
         <p class="text-center mt-4">ここにトップページのコンテンツを追加してください。</p>
     </div>
-</body>
-
-</html>
+@endsection
